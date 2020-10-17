85d1e290784995dd035283522e37f6b9aafad77d
AsnaAkram <asna_akram@hotmail.com>
2020-10-17 13:15:34 -0400

    active api

--- climate.app.py
+++ climate.app.py
@@ -1,4 +1,4 @@
-mport sqlalchemy
+import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
@@ -28,48 +28,73 @@ session = Session(engine)
 # Flask Routes
 #################################################
 
+
 @app.route("/")
 def welcome():
     """List all available api routes."""
-    return ("""<html><a href = "/api/v1.0/precipitation">/api/v1.0/precipitation</a>,
-            <a href = "/api/v1.0/<start>">/api/v1.0/<start></a>,
-            <a href = "/api/v1.0/<start>/<end>">/api/v1.0/<start>/<end></a>
-            <a href = /api/v1.0/station">/api/v1.0/station</a></html>"""
-
+    return (
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/station<br/>"
+        f"/api/v1.0/<start><br/>"
+        f"/api/v1.0/<start_dt>/<end_dt>"
     )
+
+
 session.close
 
-@app.route("/api/v1.0/precipitation)
+
+@app.route("/api/v1.0/precipitation")
 def precipitation():
 
     session = Session(engine)
     query_date = dt.date(2017, 8, 23) - dt.timedelta(days=365)
-    last_year= query_date - dt.timedelta(days=365)
+    last_year = query_date - dt.timedelta(days=365)
 
-    past_temp=session.query(measurment.date,measuremnet.prcp).filter(measurment.date<=max_date).filter(measurment)
-    !!!!!!
+    past_temp = session.query(measurement.date, measurement.prcp).filter(
+        measurement.date <= last_year).all()
 
-    rainfall={date:prcp for date, prcp in past_temp}
-    return jasonify(rainfall)
+    rainfall = {date: prcp for date, prcp in past_temp}
+    return jsonify(rainfall)
 
     session.close()
 
-    #Start of vacation page
+    # Start of vacation page
+
+
 @app.route("/api/v1.0/<start>")
-def start(start=None):
+def startdate(start):
     session = Session(engine)
-    tobs_only= (session.query(measurment.tobs).filter(measurment.date.between(start,end)).all())
+    start_only = (session.query(func.min(measurement.tobs),func.max
+                                (measurement.tobs),func.avg(measurement.tobs))).\
+                                    filter(measurement.date >= start).all()
 
-     return jsonify(tobs_only)
-     session.close()
+    return jsonify(start_only)
+    session.close()
 
-  #start page
+
+@app.route("/api/v1.0/<start_dt><end_dt>")
+def start(start_dt, end_dt):
+    session = Session(engine)
+    tobs_only = (session.query(func.min(measurement.tobs), func.max
+                               (measurement.tobs), func.avg
+                               (measurement.tobs))).filter(
+        measurement.date >= start_dt).filter(measurement.date <= end_dt).all()
+
+    return jsonify(tobs_only)
+    session.close()
+
+  # start page
+
+
 @app.route("/api/v1.0/station")
-def station():
-   station=session.query(station.name, ststion.station).all()
-   stations=list(np.ravel(ststions))
-   return jsonify(stations)
-   session.close()
+def station1():
+    session = Session(engine)
+    station_nm = session.query(station.name, station.station).all()
+    output = list(np.ravel(station_nm))
+    return jsonify(output)
+    session.close()
+
 
 if __name__ == '__main__':
     app.run(debug=True)
